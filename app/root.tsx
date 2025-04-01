a8c2f319c41024f05ff14194da7a401acf0844c3
TakuyaTaniguchi <makubex.tt@gmail.com>
2025-04-01 18:57:18 +0900

    rootの修正

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,32 @@
 import {
     Links,
+    LiveReload,
     Meta,
     Outlet,
     Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
-
 
 export default function App() {
     return (
-        <html>
-        <head>
-            <link
-                rel="icon"
-                href="data:image/x-icon;base64,AA"
-            />
-            <Meta />
-            <Links />
-        </head>
-        <body>
-        <h1>rootページ</h1>
-        <Outlet />
-
-        <Scripts />
-        </body>
+        <html lang="ja">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link
+                    rel="icon"
+                    href="data:image/x-icon;base64,AA"
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <h1>Tarot Game</h1>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
         </html>
     );
 }
