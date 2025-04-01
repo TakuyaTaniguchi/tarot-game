6e213077ef3ad38aa605938b6f94ed3d83c63925
TakuyaTaniguchi <makubex.tt@gmail.com>
2025-04-01 18:06:39 +0900

    serverファイルの設定

--- server.ts
+++ server.ts
@@ -1,31 +1,36 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
+import { createServer } from "vite";
 
-const viteDevServer =
+// ESモジュール形式で非同期に初期化
+const initServer = async () => {
+  const viteDevServer =
     process.env.NODE_ENV === "production"
-        ? null
-        : await import("vite").then((vite) =>
-            vite.createServer({
-                server: { middlewareMode: true },
-            })
-        );
+      ? null
+      : await createServer({
+          server: { middlewareMode: true },
+        });
 
-const app = express();
-app.use(
+  const app = express();
+  app.use(
     viteDevServer
-        ? viteDevServer.middlewares
-        : express.static("build/client")
-);
+      ? viteDevServer.middlewares
+      : express.static("build/client")
+  );
 
-const build = viteDevServer
+  const build = viteDevServer
     ? () =>
         viteDevServer.ssrLoadModule(
-            "virtual:remix/server-build"
+          "virtual:remix/server-build"
         )
     : await import("./build/server/index.js");
 
-app.all("*", createRequestHandler({ build }));
+  app.all("*", createRequestHandler({ build }));
 
-app.listen(3000, () => {
+  app.listen(3000, () => {
     console.log("App listening on http://localhost:3000");
-});
+  });
+};
+
+// サーバー起動
+initServer().catch(console.error);
