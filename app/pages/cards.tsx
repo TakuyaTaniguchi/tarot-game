859c65a7648e2be01ba39c74b21cbab358895f41
TakuyaTaniguchi <makubex.tt@gmail.com>
2025-04-06 18:34:21 +0900

    cardsPageを作る

---
+++ app/pages/cards.tsx
@@ -0,0 +1,10 @@
+
+
+export const CardsPage = () =>{
+
+    return (
+        <div>
+            <h1>タロット一覧</h1>
+        </div>
+    )
+}
---
+++ app/routes/cards.tsx
@@ -0,0 +1,11 @@
+import {CardsPage} from '../pages/cards'
+
+
+export default function Cards() {
+    return (
+        <div>
+            <h1>Cards Page</h1>
+            <CardsPage />
+        </div>
+    );
+}
